c9f94a26c4402a605b5bec64eab1312b4d2ab2ff
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-11-15 13:17:16 -0600

    First interaction between db and web

--- homework_board/apps/web/lib/web_web/controllers/page_controller.ex
+++ homework_board/apps/web/lib/web_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule WebWeb.PageController do
   use WebWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    %{ title: title } = Db.Assignments.get_assignment(1)
+    
+    conn
+    |> assign(:titleVar, title)
+    |> render "index.html"
   end
 end
