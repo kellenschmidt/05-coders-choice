e46ff896eb186e314e38705dc260d931b7ea2900
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-11-15 09:44:59 -0600

    Added homework db queries

--- homework_board/apps/db/lib/db.ex
+++ homework_board/apps/db/lib/db.ex
@@ -1,18 +1,3 @@
 defmodule Db do
-  @moduledoc """
-  Documentation for Db.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Db.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- homework_board/apps/db/lib/db/homework.ex
+++ homework_board/apps/db/lib/db/homework.ex
@@ -1,9 +1,56 @@
 defmodule Db.Homework do
   use Ecto.Schema
+  require Ecto.Query
 
   schema "homework" do
     field :title, :string
     field :description, :string
     field :priority, :integer
   end
+
+  def changeset(homework, params \\ %{}) do
+    homework
+    |> Ecto.Changeset.cast(params, [:title, :description, :priority])
+    |> Ecto.Changeset.validate_required([:title, :description, :priority])
+  end
+
+  def get_homework() do
+    Db.Homework
+    |> Db.Repo.all
+  end
+
+  def get_homework(id) when is_integer(id) do
+    Db.Homework
+    |> Db.Repo.get(id)
+  end
+
+  def add_homework(homework) do
+    changeset = Db.Homework.changeset(homework, %{})
+    Db.Repo.insert(changeset)
+    |> handle_query_response
+  end
+
+  def get_all_with_priority(priority) when is_integer(priority) do
+    Db.Homework |> Ecto.Query.where(priority: ^priority) |> Db.Repo.all
+  end
+
+  def update_homework(homework, newHomework) do
+    changeset = Db.Homework.changeset(homework, newHomework)
+    Db.Repo.update(changeset)
+    |> handle_query_response
+  end
+
+  def delete_homework(homework) do
+    Db.Repo.delete(homework)
+    |> handle_query_response
+  end
+
+  defp handle_query_response({ :ok, _changeset } = _response) do
+    IO.puts "Successfully modified database"
+  end
+  defp handle_query_response({ :error, _changeset } = _response) do
+    # Is raising an error proper in this situatuon?
+    raise "Error modifying database"
+  end
+
 end
