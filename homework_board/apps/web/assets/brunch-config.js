1826179d82a9766d07021ce49f819d0f5235c4c8
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-11-15 14:24:19 -0600

    Added bootstrap

--- homework_board/apps/web/assets/brunch-config.js
+++ homework_board/apps/web/assets/brunch-config.js
@@ -3,21 +3,6 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css"
