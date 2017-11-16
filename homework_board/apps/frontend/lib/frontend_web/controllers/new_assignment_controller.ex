55fdeaa44d086a9a491f635902130f444b1892ab
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-11-15 19:26:55 -0600

    Added description input for new assignment

--- homework_board/apps/frontend/lib/frontend_web/controllers/new_assignment_controller.ex
+++ homework_board/apps/frontend/lib/frontend_web/controllers/new_assignment_controller.ex
@@ -8,10 +8,13 @@ defmodule FrontendWeb.NewAssignmentController do
     |> render("index.html")
   end
 
-  def create(conn, %{"assignments" => %{"title" => title, "priority" => priority_str}} = _params) do
+  def create(conn, %{"assignments" => %{"title" => title, "description" => description, "priority" => priority_str}} = _params) do
     {priority, _} = Integer.parse(priority_str)
 
-    %Db.Assignments{title: title, priority: priority}
+    new_assignment = %Db.Assignments{}
+    |> Map.put(:title, title)
+    |> Map.put(:description, description)
+    |> Map.put(:priority, priority)
     |> Db.Assignments.add_assignment()
 
     index(conn, %{})
