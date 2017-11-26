e5fb4c97aacd81f1d275a4994bcb8049c72eddc5
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-11-26 04:30:24 -0600

    Added labels to assignment table and are updating in app

--- homework_board/apps/frontend/lib/frontend_web/controllers/new_assignment_controller.ex
+++ homework_board/apps/frontend/lib/frontend_web/controllers/new_assignment_controller.ex
@@ -10,7 +10,7 @@ defmodule FrontendWeb.NewAssignmentController do
 
   def edit(conn, %{"assignment_num" => assignment_num} = _params) do
     changeset = assignment_num
-    |> string_to_int()
+    |> to_int()
     |> Db.Assignments.get_assignment()
     |> Db.Assignments.changeset()
 
@@ -48,32 +48,39 @@ defmodule FrontendWeb.NewAssignmentController do
     |> Enum.reduce(%{}, fn ({key, val}, acc) -> Map.put(acc, String.to_atom(key), val) end)
   end
 
-  defp string_to_int(string_param) when is_bitstring(string_param) do
-    {int_representation, _} = Integer.parse(string_param)
-    int_representation
-  end
-
-  defp format_map_with_ints(%{"id" => id_str, "priority" => priority_str, "column_id" => column_id_str} = assignment_map) do
+  defp format_map_with_ints(%{"id" => id_str, "priority" => priority_str, "column_id" => column_id_str, "labels" => labels_str_list} = assignment_map) do
     assignment_map
     |> to_int_and_add_to_map("id", id_str)
     |> to_int_and_add_to_map("priority", priority_str)
     |> to_int_and_add_to_map("column_id", column_id_str)
+    |> to_int_and_add_to_map("labels", labels_str_list)
     |> map_keys_from_string_to_atom()
   end
 
-  defp format_map_with_ints(%{"priority" => priority_str, "column_id" => column_id_str} = assignment_map) do
+  defp format_map_with_ints(%{"priority" => priority_str, "column_id" => column_id_str, "labels" => labels_str_list} = assignment_map) do
     assignment_map
     |> to_int_and_add_to_map("priority", priority_str)
     |> to_int_and_add_to_map("column_id", column_id_str)
+    |> to_int_and_add_to_map("labels", labels_str_list)
     |> map_keys_from_string_to_atom()
   end
 
   defp to_int_and_add_to_map(map, key, value_str) do
     value_as_int = value_str
-    |> string_to_int()
+    |> to_int()
 
     map
     |> Map.put(key, value_as_int)
+  end
+
+  defp to_int(string_param) when is_bitstring(string_param) do
+    {int_representation, _} = Integer.parse(string_param)
+    int_representation
+  end
+
+  defp to_int(list_param) when is_list(list_param) do
+    list_param
+    |> Enum.map(&to_int/1)
   end
 
 end
