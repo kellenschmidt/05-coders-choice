0e51d5023fca7c3e64a3027ebf289675df59c083
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-11-26 02:48:51 -0600

    Handle empty columns and assignments

--- homework_board/apps/frontend/lib/frontend_web/views/new_assignment_view.ex
+++ homework_board/apps/frontend/lib/frontend_web/views/new_assignment_view.ex
@@ -1,8 +1,31 @@
 defmodule FrontendWeb.NewAssignmentView do
   use FrontendWeb, :view
 
-  def get_columns() do
+  def get_columns_list() do
     Db.Columns.get_columns()
+    |> handle_empty_columns()
+    |> Enum.map(&{&1.title, &1.id})
   end
 
+  defp handle_empty_columns({0, nil} = _empty_map) do
+    raise "Please run script to create columns"
+  end
+
+  defp handle_empty_columns(not_empty_map) do
+    not_empty_map
+  end
+
+  def get_labels_list() do
+    Db.Assignments.get_assignments()
+    |> handle_empty_assignments()
+    |> Enum.map(&{&1.title, &1.id})
+  end
+  
+  defp handle_empty_assignments({0, nil} = _empty_map) do
+    %{}
+  end
+
+  defp handle_empty_assignments(not_empty_map) do
+    not_empty_map
+  end
 end
